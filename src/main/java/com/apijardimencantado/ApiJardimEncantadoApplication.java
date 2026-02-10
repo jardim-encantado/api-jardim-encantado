45e77279b4a10d707e68985e68b048d84aefcbc5
melpiress <melissa.foneca@germinare.org.br>
2026-02-10 07:52:36 -0300

    feat: create project

---
+++ src/main/java/com/apijardimencantado/ApiJardimEncantadoApplication.java
@@ -0,0 +1,13 @@
+package com.apijardimencantado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiJardimEncantadoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiJardimEncantadoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/apijardimencantado/ApiJardimEncantadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apijardimencantado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiJardimEncantadoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
