633e8c1aac0466e4fd8b91bf3b2f8b6714f723f1
melpiress <melissa.foneca@germinare.org.br>
2026-02-21 23:27:30 -0300

    fix: adjust person role

--- src/main/java/com/apijardimencantado/service/PersonService.java
+++ src/main/java/com/apijardimencantado/service/PersonService.java
@@ -1,10 +1,13 @@
 package com.apijardimencantado.service;
 
 import com.apijardimencantado.model.database.Person;
+import com.apijardimencantado.model.database.Role;
 import com.apijardimencantado.model.dto.request.PersonRequest;
 import com.apijardimencantado.model.dto.response.PersonResponse;
 import com.apijardimencantado.model.mapper.PersonMapper;
 import com.apijardimencantado.repository.PersonRepository;
+import com.apijardimencantado.repository.RoleRepository;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -13,17 +16,29 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class PersonService extends BaseService<Person, Long, PersonRequest, PersonResponse> {
     private final PersonMapper mapper;
+    private final RoleRepository roleRepository;
 
-    public PersonService(PersonMapper personMapper, PersonRepository personRepository) {
+    public PersonService(PersonMapper personMapper, PersonRepository personRepository, RoleRepository roleRepository) {
         super(personRepository, "Person");
         this.mapper = personMapper;
+        this.roleRepository = roleRepository;
     }
 
     @Override
     @Transactional
     public PersonResponse create(PersonRequest request) {
         log.info("[PersonService] [create] CREATE");
+
         Person entity = toEntity(request);
+
+        Role role = roleRepository
+                .findById(request.roleId())
+                .orElseThrow(() ->
+                        new EntityNotFoundException("Role not found: " + request.roleId())
+                );
+
+        entity.setRole(role);
+
         return toResponse(repository.save(entity));
     }
 
