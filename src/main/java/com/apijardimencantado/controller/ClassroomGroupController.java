aab73627b009644f403a72fa17e0757ec9c01a81
MariaDawla <majudawla@hotmail.com>
2026-03-04 23:42:45 -0300

    docs: add Swagger documentation for classroom and classroomGroup

--- src/main/java/com/apijardimencantado/controller/ClassroomGroupController.java
+++ src/main/java/com/apijardimencantado/controller/ClassroomGroupController.java
@@ -1,5 +1,6 @@
 package com.apijardimencantado.controller;
 
+import com.apijardimencantado.controller.contract.ClassroomGroupContract;
 import com.apijardimencantado.model.dto.request.ClassroomGroupRequest;
 import com.apijardimencantado.model.dto.response.ClassroomGroupResponse;
 import com.apijardimencantado.service.ClassroomGroupService;
@@ -13,25 +14,29 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/classroomGroup")
 @RequiredArgsConstructor
-public class ClassroomGroupController {
+public class ClassroomGroupController implements ClassroomGroupContract {
     private final ClassroomGroupService classroomGroupService;
 
+    @Override
     @PostMapping
     public ResponseEntity<ClassroomGroupResponse> create(@RequestBody ClassroomGroupRequest classroomGroupRequest) {
         ClassroomGroupResponse response = classroomGroupService.create(classroomGroupRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
+    @Override
     @GetMapping
     public ResponseEntity<List<ClassroomGroupResponse>> getAll() {
         return ResponseEntity.ok(classroomGroupService.getAll());
     }
 
+    @Override
     @GetMapping("/{id}")
     public ResponseEntity<ClassroomGroupResponse> getById(@PathVariable Long id) {
         return ResponseEntity.ok(classroomGroupService.getById(id));
     }
 
+    @Override
     @PutMapping("/{id}")
     public ResponseEntity<ClassroomGroupResponse> update(
             @PathVariable Long id,
